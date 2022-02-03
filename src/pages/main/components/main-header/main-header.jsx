45949d01502a9e4bfe6ad03c7cfa836a68dca69d
tumorin <tumorin@gmail.com>
2022-02-03 20:54:46 +0200

    update Header with mui components

--- src/pages/main/components/main-header/main-header.jsx
+++ src/pages/main/components/main-header/main-header.jsx
@@ -1,29 +1,32 @@
 import './main-header.css';
 import {useSelector} from "react-redux";
 import {getCurrentSprint} from "../../../../redux/sprints/sprints-selectors";
+import {AppBar, Toolbar, Typography} from "@mui/material";
 
 function MainHeader() {
     const sprint = useSelector(getCurrentSprint);
     if (!sprint) return (<div>Application is loading</div>);
     return(
-        <header className="header">
-            <span className="sprint-name">{sprint.name}</span>
-            <span className="sprint-date">{sprint.start + ' - ' + sprint.end}</span>
-            <div className="sprint-legend">
-                <div className="flex-row ">
-                    <div className="circle yellow"></div>
-                    Has already expired
-                </div>
-                <div className="flex-row">
-                    <div className="circle green"></div>
-                    Completed
-                </div>
-                <div className="flex-row">
-                    <div className="circle red"></div>
-                    Canceled
-                </div>
-            </div>
-        </header>
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component={"span"}
+                        sx={{flexGrow: 1}}
+                    >
+                        {sprint.name}
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        component={"span"}
+                        sx={{flexGrow: 1}}
+                    >
+                        {`Sprint from ${sprint.start} to ${sprint.end}`}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </>
     )
 }
 
